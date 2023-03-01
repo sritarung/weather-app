eb6815758a4f3d7ce43f9ea33d3778e8d0238e60
Sebastian Csar <sacsar@gmail.com>
2023-02-28 19:31:18 -0500

    Add some deps and webpack config

---
+++ README.md
@@ -0,0 +1,8 @@
+# Assignment 3
+
+In this assignment, we're going to extend your weather page from Assignment 2
+to query an API.
+
+## Getting Set Up
+
+This repo 
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
+  devServer: {
+    static: "./dist",
+  },
+};
