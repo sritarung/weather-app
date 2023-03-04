5ef89132385050b681457ea86c74679c45f8cf5d
Sebastian Csar <sacsar@gmail.com>
2023-03-04 18:09:19 -0500

    Add skeleton for forecast.js

---
+++ src/forecast.js
@@ -0,0 +1,24 @@
+const POINT_ENDPOINT = "http://localhost:3000/weather/points";
+const FORECAST_ENDPOINT = (office, x, y) =>
+  `http://localhost:3000/weather/${office}/${x}/${y}/forecast`;
+
+/**
+ * Lookup the grid point corresponding to a given lat/long
+ *
+ * @param {number} latitude
+ * @param {number} longitude
+ */
+async function getPoint(latitude, longitude) {
+  const url = `${POINT_LOOKUP}/${latitude},${longitude}`;
+  return fetch(url);
+}
+
+/**
+ * Get the forecast for a given lat/long pair
+ *
+ * @param {number} latitude
+ * @param {number} longitude
+ */
+export default function getForecast(latitude, longitude) {
+  // TODO
+}
