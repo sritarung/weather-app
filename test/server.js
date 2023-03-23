add94e55f87facb54d15d9c140c9fa2e75424499
Sebastian Csar <sacsar@gmail.com>
2023-03-22 21:21:14 -0400

    Actually fix the test server

--- test/server.js
+++ test/server.js
@@ -1,11 +1,53 @@
 const webpack = require("webpack");
 const express = require("express");
 const devMiddleware = require("webpack-dev-middleware");
+const morgan = require("morgan");
+const fs = require("fs");
+
 const webpackConfig = require("../webpack.config");
 
 const compiler = webpack(webpackConfig);
 
+const samplePoint = {
+  properties: {
+    "@id": "https://api.weather.gov/points/39.7456,-97.0892",
+    "@type": "wx:Point",
+    cwa: "TOP",
+    forecastOffice: "https://api.weather.gov/offices/TOP",
+    gridId: "TOP",
+    gridX: 31,
+    gridY: 80,
+    forecast: "https://api.weather.gov/gridpoints/TOP/31,80/forecast",
+    forecastHourly:
+      "https://api.weather.gov/gridpoints/TOP/31,80/forecast/hourly",
+    forecastGridData: "https://api.weather.gov/gridpoints/TOP/31,80",
+    observationStations:
+      "https://api.weather.gov/gridpoints/TOP/31,80/stations",
+    relativeLocation: {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-97.086661, 39.679376],
+      },
+      properties: {
+        city: "Linn",
+        state: "KS",
+        distance: {
+          unitCode: "wmoUnit:m",
+          value: 7366.9851976444,
+        },
+        bearing: {
+          unitCode: "wmoUnit:degree_(angle)",
+          value: 358,
+        },
+      },
+    },
+  },
+};
+const sampleForecast = JSON.parse(fs.readFileSync("src/sample_forecast.json"));
+
 const app = express();
+
 const response = {
   result: {
     input: {
@@ -41,14 +83,18 @@ const response = {
   },
 };
 
-app.use(devMiddleware(compiler));
+app.use(devMiddleware(compiler), morgan("dev"));
 
 app.get(/address/, (req, res) => {
   res.send(response);
 });
 
+app.get(/weather\/points/, (req, res) => {
+  res.send(samplePoint);
+});
+
 app.get(/weather/, (req, res) => {
-  res.send({});
+  res.send(sampleForecast);
 });
 
 app.listen(3000, () => console.log("Express listening on port 3000"));
